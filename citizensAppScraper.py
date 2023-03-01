cf8ae694b761017c38f5bdf0e835ab7ea5355de5
tlus3r <tommylu3h@gmail.com>
2023-03-01 13:01:56 -0500

    s

--- citizensAppScraper.py
+++ citizensAppScraper.py
@@ -4,51 +4,61 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from datetime import datetime, timedelta
 from sqlalchemy import create_engine
-from secrets import sqlURL
+from secretsKey import sqlURL
+import time
+import schedule
 
-datesList = []
-titlesList = []
-neighborhoodsList = []
-locationsList = []
 
-driver = webdriver.Chrome('/Users\Tommy\Documents\python\chromedriver')
-driver.get("https://citizen.com/explore")
-driver.minimize_window()
-driver.implicitly_wait(2)
-dates = driver.find_elements(By.CLASS_NAME, 'date')
-titles = driver.find_elements(By.CLASS_NAME, 'title')
-neighborhoods = driver.find_elements(By.CLASS_NAME, 'neighborhood')
-locations = driver.find_elements(By.CLASS_NAME, 'location')
+def webScrape():
+    datesList = []
+    titlesList = []
+    neighborhoodsList = []
+    locationsList = []
 
-for i in titles:
-    titlesList.append(i.text)
-for i in locations:
-    locationsList.append(i.text)
-for i in neighborhoods:
-    neighborhoodsList.append(i.text)
-for i in dates:
-    today = datetime.today()
-    yesterday = str((today-timedelta(days=1)).strftime('%Y-%m-%d'))
-    i = i.text
-    if 'Yesterday' in i:
-        i = i.replace('Yesterday', yesterday)
-        datesList.append(i)
-    else:
-        today = str(today.strftime('%Y-%m-%d'))
-        i = today + " " + i
-        datesList.append(i)
-driver.quit()
+    driver = webdriver.Chrome()
+    driver.get("https://citizen.com/explore")
+    driver.minimize_window()
+    driver.implicitly_wait(2)
+    dates = driver.find_elements(By.CLASS_NAME, 'date')
+    titles = driver.find_elements(By.CLASS_NAME, 'title')
+    neighborhoods = driver.find_elements(By.CLASS_NAME, 'neighborhood')
+    locations = driver.find_elements(By.CLASS_NAME, 'location')
 
-#dataframe
-d = {'date': datesList, 'title': titlesList,
-     'location': locationsList, 'neighborhood': neighborhoodsList}
-df = pd.DataFrame(data=d)
+    for i in titles:
+        titlesList.append(i.text)
+    for i in locations:
+        locationsList.append(i.text)
+    for i in neighborhoods:
+        neighborhoodsList.append(i.text)
+    for i in dates:
+        today = datetime.today()
+        yesterday = str((today-timedelta(days=1)).strftime('%Y-%m-%d'))
+        i = i.text
+        if 'Yesterday' in i:
+            i = i.replace('Yesterday', yesterday)
+            datesList.append(i)
+        elif 'Yesterday' not in i:
+            today = str(today.strftime('%Y-%m-%d'))
+            i = today + " " + i
+            datesList.append(i)
+        else:
+            pass
+    driver.quit()
 
-#connection to postgresql/uploading scraped data
-conn_string = sqlURL
-db = create_engine(conn_string)
-conn = db.connect()
-df.to_sql('crimes', con=conn, if_exists='append',index=False)# current issue is database will upload duplicates
-conn.close()
+    #dataframe
+    d = {'date': datesList, 'title': titlesList,
+        'location': locationsList, 'neighborhood': neighborhoodsList}
+    df = pd.DataFrame(data=d)
+    print(df)
+    #connection to postgresql/uploading scraped data
+    conn_string = sqlURL
+    db = create_engine(conn_string)
+    conn = db.connect()
+    df.to_sql('crimes', con=conn, if_exists='append',index=False)# current issue is database will upload duplicates
+    conn.close()
+    print('success')
 
-print('success')
+schedule.every(1).minute.do(webScrape)
+while True:
+    schedule.run_pending()
+    time.sleep(1) # wait one minute
